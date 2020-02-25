79837f51477a2e093280969acf30726c36d90746
M Grimes <matthewlgrimes@gamil.com>
2020-02-25 14:23:42 -0700

    fixed loading problem

--- server.js
+++ server.js
@@ -11,57 +11,59 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
 
-app.get('/notes', function(req, res) {
+app.get('/notes', function (req, res) {
     res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
 // Adds a new note
-app.post('/api/notes', function(req, res) {
+app.post('/api/notes', function (req, res) {
     var newNote = req.body;
     newNote.id = id;
     //Read file
-    fs.readFile('./db/db.json', 'utf8', function(err, data) {
+    fs.readFile('./db/db.json', 'utf8', function (err, data) {
         if (err) throw err;
         let notes = JSON.parse(data);
         notes.push(newNote);
         // Write new file
-        fs.writeFile('./db/db.json', JSON.stringify(notes), function(err){
+        fs.writeFile('./db/db.json', JSON.stringify(notes), function (err) {
             if (err) throw err;
         });
         id += 1;
     });
+    res.send(200);
 });
 
 // Gets all notes
-app.get('/api/notes', function(req, res) {
-    fs.readFile('./db/db.json', 'utf8', function(err, data) {
+app.get('/api/notes', function (req, res) {
+    fs.readFile('./db/db.json', 'utf8', function (err, data) {
         if (err) throw err;
         res.send(JSON.parse(data));
     });
 });
 
 // Uses Id to filter notes array
-app.delete('/api/notes/:id', function(req, res) {
+app.delete('/api/notes/:id', function (req, res) {
     let deleteId = req.params.id;
-    fs.readFile('./db/db.json', 'utf8', function(err, data){
+    fs.readFile('./db/db.json', 'utf8', function (err, data) {
         if (err) throw err;
         notes = JSON.parse(data);
-        let newNotes = notes.filter(note => note.id === deleteId);
-        fs.writeFile('./db/db.json', JSON.stringify(newNotes), function(err) {
+        var newNotes = notes.filter(test => test.id === +deleteId);
+        fs.writeFile('./db/db.json', JSON.stringify(newNotes), function (err) {
             if (err) throw err;
         });
     });
+    res.send(200);
 });
 
 // Default path
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.get('*', function(req, res) {
+app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
